1b9bd4a3237af6e86220b3d0b515c1d8cfe7d4f9
Bran2Joestar <r3s3nd1z@hotmail.com>
2025-05-12 09:40:16 -0600

    Instalacion de mas dependencias
    Instalación de Axios y Vue-Cookies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue';
+import axios from 'axios';
+import VueCookies from 'vue-cookies';
 
+Vue.use(VueCookies);
+Vue.prototype.$http = axios;
 createApp(App).mount('#app')
 //  wasas
